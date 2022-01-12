d17cb9348ab31fc94a596e62297be62e37603dd1
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-12 13:07:12 -0300

    Adicionando funcionalidades e botão de Enviar em contato.html

--- atividade2/assets/js/script.js
+++ atividade2/assets/js/script.js
@@ -2,6 +2,12 @@ let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let mensagem = document.querySelector('#mensagem')
 
+let nomeOk = false
+let emailOk = false
+let mensagemOk = false
+
+let botao = document.querySelector("#botao")
+
 function validaNome(){
     let txtNome = document.querySelector('#txtNome')
     
@@ -11,6 +17,7 @@ function validaNome(){
     } else {
         txtNome.innerHTML = '<p>Nome válido</p>'
         txtNome.style.color = 'green'
+        nomeOk = true
     }
 }
 
@@ -23,20 +30,41 @@ function validaEmail(){
     } else {
         txtEmail.innerHTML = '<p>E-mail válido</p>'
         txtEmail.style.color = 'green'
+        emailOk = true
     }
 }
 
 function validaMensagem(){
     let txtMensagem = document.querySelector('#txtMensagem')
 
+    txtMensagem.style.display = 'block'
+
     if(mensagem.value.length < 10){
-        txtMensagem.innerHTML = '<p>Mensagem muito pequena. Digite mais!</p>'
+        txtMensagem.innerHTML = '<p>Digite mais para poder enviar.</p>'
         txtMensagem.style.color = 'gray'
     } else if (mensagem.value.length > 50){
         txtMensagem.innerHTML = '<p>Texto muito grande. Digite menos de 100 caracteres.</p>'
         txtMensagem.style.color = 'red'
     } else {
-        txtMensagem.innerHTML = '<p>Mensagem já pode ser enviada!</p>'
-        txtMensagem.style.color = 'green'
+        txtMensagem.style.display = 'none'
+        mensagemOk = true
     }
+}
+
+function enviar(){
+    if(nomeOk && emailOk && mensagemOk){
+        alert('Formulário enviado com sucesso!')
+    } else {
+        alert('Preencha todo o formulário antes de enviar!')
+    }
+}
+
+function botaoOn(){
+    botao.style.backgroundImage = 'linear-gradient(to right, rgb(34, 34, 34), black 50%, rgb(34, 34, 34))'
+    botao.style.boxShadow = '4px 4px 5px black'
+}
+
+function botaoOff(){
+    botao.style.backgroundImage = 'linear-gradient(to right, black, black)'
+    botao.style.boxShadow = '2px 2px 2px black'
 }
